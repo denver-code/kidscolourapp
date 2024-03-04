a560397b3a81e62796ff5a1ad8084272789b6ee8
Ihor Savenko <csigorek@gmail.com>
2024-03-04 10:21:34 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,15 @@ import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
+// Entry-point of the app
 void main() {
   runApp(
     GetMaterialApp(
+      // Setting title of the App
       title: "Kid Colour App",
+      // Setting initial route/page
       initialRoute: AppPages.INITIAL,
+      // Set all the available pages to the app
       getPages: AppPages.routes,
     ),
   );
